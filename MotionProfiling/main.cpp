14b9ecc66c854889d80a68b90abd945496d4a482
pianist1119 <pianist1119@gmail.com>
2017-02-02 16:47:11 -0500

    Added some 550 stuff

--- MotionProfiling/main.cpp
+++ MotionProfiling/main.cpp
@@ -3,26 +3,58 @@
 
 using namespace std;
 
-//#define _USE_DEBUG_VARIBLES_
+#define _USE_DEBUG_VARIABLES_ true
+
+#define _USE_DEBUG_OUTPUT_ true
+
+
+// Different types of motors, for generating acceleration profiles
+#define _USE_BANEBOT_550_ true
 
 
 
 
-#ifndef _USE_DEBUG_VARIABLES_
+#ifndef _USE_DEBUG_VARIABLES_ // Instead, use the motor variables
+
+#ifdef _USE_BANEBOT_550_
+
+// Units of rpm
+
+const double MAXIMUM_JERK = 500;
+const double MAXIMUM_ACCELERATION = 1000;
+const double TARGET_VELOCITY = 15000;
+const double TARGET_DISTANCE = 100;
+const double PRECISION = 128;
+const double timeFactor = 1;
+
+#else
+
+
+
 const double MAXIMUM_JERK = 426.67;
 const double MAXIMUM_ACCELERATION = 853.333;
 const double TARGET_VELOCITY = 15000;
 const double TARGET_DISTANCE = 20;
 const double PRECISION = 128;
 const double timeFactor = 10;
-#else
+
+
+
+#endif // _USE_BANEBOT_550_
+
+
+#elif _USE_DEBUG_VARIABLES_
+
+
 const double MAXIMUM_JERK = 1;
 const double MAXIMUM_ACCELERATION = 2;
-const double MAXIMUM_VELOCITY = 5;
-const double TARGET_DISTANCE = 50;
+const double TARGET_VELOCITY = 5;
+const double TARGET_DISTANCE = 2;
 const double PRECISION = 128;
 const double timeFactor = 1;
-#endif
+
+
+#endif // _USE_DEBUG_VARIABLES_
 
 
 int main()
@@ -33,16 +65,20 @@ int main()
 
 	points = profile.Populate();
 
-	std::cout << "Acceleration,Velocity,Position,Time" << endl;
+	std::cout << "Acceleration,Velocity,Position" << endl;
 
 	double timeIncrement = points[points.size() - 1].Time / PRECISION;
 
 	for (int i = 0; i < points.size(); ++i)
 	{
+#ifndef _USE_DEBUG_OUTPUT_
 		std::cout << "{ " << points[i].Position << " , " << points[i].Velocity << " , " << timeIncrement*timeFactor << " }";
 		if (i != points.size() - 1)
 			std::cout << ",";
 		std::cout << endl;
+#else
+		std::cout << points[i].Acceleration << "," << points[i].Velocity << "," << points[i].Position << endl;
+#endif // _USE_DEBUG_OUTPUT_
 	}
 
 	system("pause");
